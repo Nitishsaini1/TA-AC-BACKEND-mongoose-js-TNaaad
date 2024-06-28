848782b76daa1cccc2becbd4d21e9fe7f3b70246
Nitish Kumar <ns7410995@gmail.com>
2024-06-29 03:22:51 +0530

    Explain the code

---
+++ block-BNaabz/sample/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/mydatabase', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.error('Error connecting to MongoDB', err);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello, MongoDB!');
+});
+
+app.listen(3000, () => {
+  console.log(`Server is running on port 3k`);
+});
